c93ae77f5e62e16ca2969fab9a9cc840c64cf492
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-06-17 15:57:57 -0300

    Add table initialization and scene switching functionality to TelaProdutosController

--- src/fxcontroles/TelaProdutosController.java
+++ src/fxcontroles/TelaProdutosController.java
@@ -4,9 +4,12 @@
  */
 package fxcontroles;
 
+import DAO.ProdutosDAO;
+import entidades.Produtos;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,33 +19,64 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javax.xml.transform.Source;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
  *
  * @author gusta
  */
-public class TelaPrincipalController implements Initializable {
-    
+public class TelaProdutosController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    
+    private ProdutosDAO banco;
     @FXML
-    private Button TelaVendedores;
+    private Button novoProdBtn;
     @FXML
-    private Button TelaProdutos;
+    private TableView<Produtos> tableProdutos;
+    @FXML
+    private TableColumn<Produtos, String> descCol;
+    @FXML
+    private TableColumn<Produtos, Double> precoCol;
+    @FXML
+    private TableColumn<Produtos, String> imgCol;
+    @FXML
+    private TableColumn<Produtos, String> medidaCol;
+    @FXML
+    private TableColumn<Produtos, Integer> idCol;
+    @FXML
+    private Button TelaPrincipal;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            banco = new ProdutosDAO();    // TODO
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro na inicialização do banco",
+                    JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+        initializeTable();
     }    
     
+     private void initializeTable(){
+        idCol.setCellValueFactory(new PropertyValueFactory<>("codproduto"));
+        descCol.setCellValueFactory(new PropertyValueFactory<>("descProduto"));
+        precoCol.setCellValueFactory(new PropertyValueFactory<>("preco"));
+        imgCol.setCellValueFactory(new PropertyValueFactory<>("imagem"));
+        medidaCol.setCellValueFactory(new PropertyValueFactory<>("unidadeMedida"));
+        
+        tableProdutos.setItems(FXCollections.observableArrayList(banco.getAll()));
+    }
+     
     @FXML
     public void switchScene(ActionEvent event)  {
         try{
@@ -69,5 +103,4 @@ public class TelaPrincipalController implements Initializable {
             
         }
     }
-    
 }
